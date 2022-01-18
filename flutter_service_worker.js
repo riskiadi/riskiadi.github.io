3c0364e9a86e24c38bdeb43e211202fe4a1b38fd
Rizki Adi Saputra <rizkiadisaputra77@gmail.com>
2022-01-18 08:15:53 +0700

    test admin page

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "70a5f4e0afd0ae3d6cf16c371fbb0477",
-"/": "70a5f4e0afd0ae3d6cf16c371fbb0477",
-"main.dart.js": "ad343baace350e7287d19f6a9ea259b4",
+"index.html": "60bf10880da6193fb91d8575e8beb9ea",
+"/": "60bf10880da6193fb91d8575e8beb9ea",
+"main.dart.js": "41dcce91e2ec439687792da740be4358",
 "manifest.json": "7d17eeca8f794450b0bea9f26f3091f8",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
