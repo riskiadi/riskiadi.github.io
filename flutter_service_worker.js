d2679bb9f95d50790bfe9701f7c98dd54c6cba5f
Rizki Adi Saputra <ucupucupucup10@gmail.com>
2022-06-19 08:49:49 +0700

    bug fix certificate

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "972a3b341bc14e4ad93af95a3a2e7000",
-"/": "972a3b341bc14e4ad93af95a3a2e7000",
-"main.dart.js": "232c058666f5b249c3ce72f259724819",
+"index.html": "1dab153c0d1b19a0c8abb37996b291bc",
+"/": "1dab153c0d1b19a0c8abb37996b291bc",
+"main.dart.js": "d5d0e45031d3b10d85184da94b58ec3d",
 "manifest.json": "7d17eeca8f794450b0bea9f26f3091f8",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
